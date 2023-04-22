74d8b83c66a5a719443f94b5665e38f287093056
Hidetoshi Hibino <hibinozzzz@gmail.com>
2023-04-23 01:10:05 +0900

    ルーティングの開始とグローバルレイアウトの実装

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+
+import Root from './routes/root';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Root />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* This first route is what we often call the "root route" 
+    since the rest of our routes will render inside of it. 
+    It will serve as the root layout of the UI, 
+    we'll have nested layouts as we get farther along. */}
   </React.StrictMode>,
 )
