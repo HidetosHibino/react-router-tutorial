0a47324c2300c8fa1c2a17321f819288707ea7af
Hidetoshi Hibino <hibinozzzz@gmail.com>
2023-04-23 14:10:57 +0900

    Updating Data

--- src/main.jsx
+++ src/main.jsx
@@ -17,6 +17,7 @@ import {
 } from './routes/contact'
 import ErrorPage from './error-page';
 import Contact from './routes/contact';
+import EditContact from './routes/edit';
 
 const router = createBrowserRouter([
   {
@@ -32,6 +33,11 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
         loader: contactLoader
       },
     ]
