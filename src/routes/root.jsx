53238a1100208f124b42dc0b351f59dc3610dc65
Hidetoshi Hibino <hibinozzzz@gmail.com>
2023-04-23 17:33:34 +0900

    Adding Search Spinner

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -39,8 +39,16 @@ export default function Root(){
     document.getElementById("q").value = q;
   }, [q])
 
+  const searching = 
+    navigation.location && 
+    new URLSearchParams(navigation.location.search).has(
+      "q"
+    );
+
   return (
     <>
+      {/* check navigation */}
+      {console.log(navigation.location)}
       <div id="sidebar">
         <h1>React Router Contacts</h1>
         <div>
@@ -49,6 +57,7 @@ export default function Root(){
           <Form id="search-form" role="search">
             <input
               id="q"
+              className={searching ? "loading" : ""}
               aria-label="Search contacts"
               placeholder="Search"
               type="search"
@@ -62,7 +71,7 @@ export default function Root(){
             <div
               id="search-spinner"
               aria-hidden
-              hidden={true}
+              hidden={!searching}
             />
             <div
               className="sr-only"
