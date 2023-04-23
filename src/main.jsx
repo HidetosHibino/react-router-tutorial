e9943011e4d0e5bb44717f935e87a872be12a039
Hidetoshi Hibino <hibinozzzz@gmail.com>
2023-04-23 19:22:01 +0900

    Pathless Routes

--- src/main.jsx
+++ src/main.jsx
@@ -35,30 +35,35 @@ const router = createBrowserRouter([
     // That means all of your useLoaderData hooks update and the UI stays in sync with your data automatically! Pretty cool.
     action: rootAction,
     children: [
-      // Note the { index:true } instead of { path: "" }.
-      // That tells the router to match and render this route when the user is at the parent route's exact path, so there are no other child routes to render in the <Outlet>.
-      { index: true, element: <Index />},
       {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader: contactLoader,
-        action: contactAction
-      },
-      {
-        path: "contacts/:contactId/edit",
-        element: <EditContact />,
-        loader: contactLoader,
-        action: editAction
-      },
-      {
-        path: "contacts/:contactId/destroy",
-        action: destroyAction,
-        // Because the destroy route has its own errorElement and is a child of the root route, 
-        // the error will render there instead of the root. As you probably noticed, 
-        // these errors bubble up to the nearest errorElement. 
-        // Add as many or as few as you like, as long as you've got one at the root.
-        // 子供にerrorElementをしていないと親に吸われる
-        errorElement: <div>Oops! There was an error.</div>,
+        errorElement: <ErrorPage />,
+        children: [
+          // Note the { index:true } instead of { path: "" }.
+          // That tells the router to match and render this route when the user is at the parent route's exact path, so there are no other child routes to render in the <Outlet>.
+          { index: true, element: <Index />},
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            // Because the destroy route has its own errorElement and is a child of the root route, 
+            // the error will render there instead of the root. As you probably noticed, 
+            // these errors bubble up to the nearest errorElement. 
+            // Add as many or as few as you like, as long as you've got one at the root.
+            // 子供にerrorElementをしていないと親に吸われる
+            errorElement: <div>Oops! There was an error.</div>,
+          },
+        ]
       },
     ]
   },
